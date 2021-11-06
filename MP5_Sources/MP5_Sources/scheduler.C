78dc87d44c394d78f1362925a12432305ae0bba2
philip0227 <philip0227@tamu.edu>
2021-11-06 12:12:54 -0500

    should revise

--- MP5_Sources/MP5_Sources/scheduler.C
+++ MP5_Sources/MP5_Sources/scheduler.C
@@ -46,22 +46,93 @@
 /*--------------------------------------------------------------------------*/
 
 Scheduler::Scheduler() {
-  assert(false);
+  current_running_thread = NULL;
+  head = NULL;
+  tail = NULL;
   Console::puts("Constructed Scheduler.\n");
 }
 
 void Scheduler::yield() {
-  assert(false);
+  if(head!=NULL)
+  {
+	  queue* temp = head;
+	  Thread* next_thread = head->thread;
+	  head = head->next;
+	  if(head==NULL)
+	  {
+		  tail=NULL;
+	  }
+	  current_running_thread = next_thread;
+	  Thread::dispatch_to(next_thread);
+  }
 }
 
 void Scheduler::resume(Thread * _thread) {
-  assert(false);
+  enqueue(_thread);
 }
 
 void Scheduler::add(Thread * _thread) {
-  assert(false);
+  enqueue(_thread);
+
 }
 
 void Scheduler::terminate(Thread * _thread) {
-  assert(false);
+ // running thread sucide
+  if (current_running_thread == _thread)
+  {
+	 yield();
+  }
+  else
+  {
+  	if(head->thread == _thread) // if terminate head
+  	{
+	  	head = head->next;
+	  	if(head==NULL)
+	  	{
+	  		tail = NULL;
+	  	}
+  	}
+  	else
+  	{
+  		queue* current = head->next;
+  		queue* prev = head;
+  		while(current!=NULL)
+  		{
+	  		if (current->thread !=_thread)
+	  		{
+		  		current = current->next;
+		 		prev = prev->next;
+	  		}
+	  		else
+	  		{
+				prev->next = current->next;
+				current = current->next;
+			}
+		}
+  	}
+  }
 }
+
+void Scheduler::enqueue(Thread* _thread)
+{
+	if(head==NULL||tail==NULL)
+	{
+		queue new_thread;
+		new_thread.thread = _thread;
+		new_thread.next = NULL;
+		head = &new_thread;
+		tail = &new_thread;
+	}
+	else
+	{
+		queue new_thread;
+		new_thread.thread = _thread;
+		new_thread.next = NULL;
+		tail->next = &new_thread;
+		tail = tail->next;
+	}
+}
+
+	
+
+
